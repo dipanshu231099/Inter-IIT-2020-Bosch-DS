ca36c071c7838eba404c02acce12b9d08bae44d3
nidhijainpnp <12njain06@gmail.com>
2021-03-15 19:00:41 +0530

    frontend for aumentation

--- classification/urls.py
+++ classification/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.addTrainingImage, name='index'),
     path('graphs/', views.index, name='plotly_graghs'),
+    path('augment/', views.augment, name='augmentInput'),
 ]
