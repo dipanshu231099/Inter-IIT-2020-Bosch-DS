154a6cf0306bfcf5b8d833cb5f82d7df2ff99c2f
Srishtiginjala <srishti.ginjala@gmail.com>
2021-03-15 12:38:16 +0530

    Added sample plotly graph

--- classification/urls.py
+++ classification/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.addTrainingImage, name='index'),
+    path('graphs/', views.index, name='plotly_graghs'),
 ]
--- classification/views.py
+++ classification/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
-from .forms import UploadTrainImage 
+from .forms import UploadTrainImage
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
+
 
 def addTrainingImage(request):
     if request.method == 'POST':
@@ -22,5 +25,11 @@ def testImage():
     pass
 
 
-
-
+def index(request):
+    x_data = [0,1,2,3]
+    y_data = [x**2 for x in x_data]
+    plot_div = plot([Scatter(x=x_data, y=y_data,
+                        mode='lines', name='test',
+                        opacity=0.8, marker_color='green')],
+               output_type='div')
+    return render(request, "index.html", context={'plot_div': plot_div})
